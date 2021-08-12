f246da952c0bd15e06c11fa2e0e161f2cbcaa51f
Stap <marsik200894>
2021-08-12 12:26:42 +0600

    Koin api(12:26)

---
+++ app/src/main/java/com/example/testkoin/servise/AuthInterceptor.kt
@@ -0,0 +1,20 @@
+package com.example.testkoin.servise
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val newUrl = chain.request().url
+                .newBuilder()
+                .build()
+
+        val newRequest = chain.request()
+                .newBuilder()
+                .addHeader("Authorization", "bearer ")
+                .url(newUrl)
+                .build()
+
+        return chain.proceed(newRequest)
+    }
+}
